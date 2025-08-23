cbc31f5f105aa5beee4d269f4fc753298c8b8c2c
Shahidul Islam <shahidulb15@gmail.com>
2025-08-23 16:08:51 +0600

    add new formate

--- Number Counter- project/javascript.js
+++ Number Counter- project/javascript.js
@@ -1,13 +1,22 @@
-let increment = document.querySelector("#increase");
-let decrement = document.querySelector("#decrease");
+// let increment = document.querySelector("#increase");
+// let decrement = document.querySelector("#decrease");
+// let counter = document.querySelector("#counter");
+// let count = 0;
+
+// increment.onclick = function () {
+//   count++;
+//   counter.textContent = count; // Update the displayed count
+// };
+// decrement.onclick = function () {
+//   count--;
+//   counter.textContent = count; // Update the displayed count
+// };
+
+//Alternative formula
 let counter = document.querySelector("#counter");
 let count = 0;
 
-increment.onclick = function () {
-  count++;
-  counter.textContent = count; // Update the displayed count
-};
-decrement.onclick = function () {
-  count--;
-  counter.textContent = count; // Update the displayed count
-};
+document.querySelector("#increase").onclick = () =>
+  (counter.textContent = ++count);
+document.querySelector("#decrease").onclick = () =>
+  (counter.textContent = --count);
